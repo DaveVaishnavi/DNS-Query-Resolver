4d3efc8cb00117036609bb597da747f387e9f78f
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2024-10-14 12:12:31 +0530

    feat: initialize flask app

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from recursive_dns_query import recursive_dns_query
 from iterative_dns_query import iterative_dns_query
 from cache import Cache
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
-def main():
-    domain = input("Enter the domain to resolve: ")
+# Homepage route
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
 
+@app.route('/resolve', methods=['POST'])
+def resolve():
+    domain = request.form.get('domain')
     cache = Cache("cache.json")
+    try:
+        ip_recursive = recursive_dns_query(domain, cache)
+        ip_iterative = iterative_dns_query(domain, cache)
+    except Exception as e:
+        ip_recursive = f"Invalid domain or unable to resolve due to {e}"
+        ip_iterative = f"Invalid domain or unable to resolve due to {e}"
+    return render_template('index.html', domain=domain, ip_recursive=ip_recursive, ip_iterative=ip_iterative)
 
-    # Recursive DNS Query
-    ip_recursive = recursive_dns_query(domain, cache)
-    print(f"Resolved IP (recursive): {ip_recursive}")
-
-    # Iterative DNS Query
-    ip_iterative = iterative_dns_query(domain, cache)
-    print(f"Resolved IP (iterative): {ip_iterative}")
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
